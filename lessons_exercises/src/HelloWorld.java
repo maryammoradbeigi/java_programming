681e38415467aca5dccf74f6ea7950ccb9157b97
maryammoradbeigi <maryammoradbeygi@gmail.com>
2024-02-21 21:47:00 +0100

    Add lesson notes in README and Add comments in the code

--- lessons_exercises/src/HelloWorld.java
+++ lessons_exercises/src/HelloWorld.java
@@ -1,4 +1,7 @@
-public class HelloWorld {
+public class HelloWorld { //the class should be inside two slashes
+    /* A standalone java program must have main function
+    This is used as entry point and allows the program to run
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
