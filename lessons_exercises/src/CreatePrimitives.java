0688c28275d55b7b89efa27ad0618d21af82df27
maryammoradbeigi <maryammoradbeygi@gmail.com>
2024-02-28 20:05:55 +0100

    Clean up the scrip

--- lessons_exercises/src/CreatePrimitives.java
+++ lessons_exercises/src/CreatePrimitives.java
@@ -11,7 +11,9 @@ public class CreatePrimitives {
 
         // Creating an array with 12 int
         int[] daysInMonths = new int[12];
+        // Creating array of strings and assigning values immediately
         String[] monthNames = {"Jan", "Feb", "Mar", "apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        // assigning the value of each element of daysInMonths array
         daysInMonths[0] = 31;
         daysInMonths[1] = 28;
         daysInMonths[2] = 31;
@@ -24,6 +26,8 @@ public class CreatePrimitives {
         daysInMonths[9] = 31;
         daysInMonths[10] = 30;
         daysInMonths[11] = 31;
+        // create primitive x and assign the lengths of daysInMonths to it
+        int x = daysInMonths.length;
 
         // assign values to width, height, area
         width = 8;
@@ -48,17 +52,6 @@ public class CreatePrimitives {
         System.out.println(monthNames[9] + " has " + daysInMonths[0] + " days.");
         System.out.println(monthNames[10] + " has " + daysInMonths[0] + " days.");
         System.out.println(monthNames[11] + " has " + daysInMonths[0] + " days.");
+        System.out.println("The number of months in a year is " + x);
     }
 }
-//// Creating an array of integer with the length of 25
-//int[] myIntArray = new int[25];
-//// assigning the first element the value 42
-//myIntArray[0] = 42;
-//
-//// Creating an array of integer and assign values to its elements
-//int[] myIntArray = {11, 12, 13, 14, 15};
-//// Creating an array of strings
-//String[] pets = {"Fido", "REx", "Princess"};
-//
-//// Getting the lengths of array myIntArray and assign it to int x
-//int x = myIntArray.length;
